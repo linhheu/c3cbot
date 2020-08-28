8b0ffdd611d5b75188d18bb8b017f3676cf9ba3f
lequanglam <29724654+lequanglam@users.noreply.github.com>
2020-08-28 10:07:00 +0700

    NPM for plugins (not tested yet)

--- app/npmHandler.js
+++ app/npmHandler.js
@@ -1,21 +1,58 @@
-/*
-  mirai tuổi lồn 
-    
-    -UIRI 2020
-*/
-
 let fs = require("fs");
 let path = require("path");
 let semver = require("semver");
+let childProcess = require("child_process");
 
 const moduleDir = path.join(process.cwd(), ".data", "node_modules_plugins");
 const tempDir = path.join(process.cwd(), ".data", "temp_npm_pack");
+const emptyPKGJSON = '{\
+  "name": "c3cplnpm",\
+  "version": "1.0.0",\
+  "description": "",\
+  "main": "index.js",\
+  "scripts": {\
+    "test": "exit 0"\
+  },\
+  "author": "",\
+  "license": "GPL-3.0"\
+}';
 global.ensureExists(moduleDir);
 global.ensureExists(tempDir);
 
 
 let downloadPackage = async function downloadPackage(packageName, versionRange) {
-
+    await fs.promises.writeFile(path.join(tempDir, "package.json"), emptyPKGJSON);
+    try {
+        await new Promise((resolve, reject) => {
+            let p = childProcess.spawn("npm", ["install", `${packageName}@${versionRange}`], {
+                cwd: tempDir
+            });
+            p.once("exit", async (code, signal) => {
+                if (code !== 0 && signal !== null) {
+                    return reject([code, signal]);
+                }
+                await fs.promises.rename(
+                    path.join(tempDir, "node_modules", packageName), 
+                    path.join(
+                        moduleDir,
+                        packageName,
+                        (await JSON.parse(fs.promises.readFile(path.join(tempDir, "node_modules", packageName, "package.json")))).version
+                    )
+                )
+                try {
+                    await fs.promises.rmdir(path.join(tempDir, "node_modules"), {
+                        recursive: true
+                    });
+                    await fs.promises.unlink(path.join(tempDir, "package.json"));
+                } catch (_) {
+                    return reject([null, null, _]);
+                }
+                resolve();
+            });
+        });
+    } catch (ex) {
+        throw new Error("No such package exists.");
+    }
 }
 
 let checkPackageExists = async function checkPackageExists(packageName, versionRange) {
@@ -33,9 +70,9 @@ let checkPackageExists = async function checkPackageExists(packageName, versionR
                 .filter(v => v.isDirectory()).map(v => v.name)
                 .sort((vA, vB) => semver.compare(vA, vB))
                 .reverse();
-            
+
             for (let version of versionList) {
-                if (semver.satisfies(version, versionRange)) return true;
+                if (semver.satisfies(version, versionRange)) return version;
             }
 
             return false;
@@ -55,8 +92,16 @@ let checkPackageExists = async function checkPackageExists(packageName, versionR
  * @returns {Promise<any>} module.exports of that package
  */
 module.exports = async function loadPackage(packageName, versionRange) {
-    let packageExists = await checkPackageExists(packageName, versionRange);
-    if (!packageExists) downloadPackage(packageName, versionRange);
-    
-    
+    for (; ;) {
+        let version = await checkPackageExists(packageName, versionRange);
+        if (!version) await downloadPackage(packageName, versionRange);
+        let rtData;
+        try {
+            rtData = require(path.join(moduleDir, packageName, version));
+        } catch (_) {
+            await fs.promises.rmdir(path.join(moduleDir, packageName, version), { recursive: true });
+            continue;
+        }
+        return rtData;
+    }
 }
